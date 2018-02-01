ee5281f4bac048fda56a0c12b4864c2b439dce73
Al Anderson <al_anderson@skc.edu>
2018-01-31 21:28:07 -0700

    Added search_bar and update app.js to render search_bar

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
+import SearchBar from './search_bar'
+
+const API_KEY = 'AIzaSyCJH8Y_ZExpA6SMLvSn2i0zV-R1EPYcnZA';
+
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
