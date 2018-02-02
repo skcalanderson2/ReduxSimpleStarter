190566fe1f45c09d10a6efb90d332c185ec81775
Al Anderson <skcalanderson@gmail.com>
2018-02-02 08:06:13 -0700

    Added video thumbnails and titles to video_list_item

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 const VideoListItem = ({video}) => {
   //const video = props.videos;
+  const imageURL = video.snippet.thumbnails.default.url;
 
-  return <li>Video</li>
+  return (
+    <li className = "list-group-item">
+      <div className = "video-list media">
+        <div className = "media-left">
+          <img className = "media-object" src = {imageURL}/>
+        </div>
+
+        <div className = "media-body">
+          <div className = "media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  )
 };
 
 export default VideoListItem;
