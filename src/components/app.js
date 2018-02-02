07c7fe7f862a70163858bf1d69a7327d5fb96300
Al Anderson <al_anderson@skc.edu>
2018-02-01 21:20:43 -0700

    slight changes to app.js and search_bar.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-
+import YTSearch from 'youtube-api-search'
 import SearchBar from './search_bar'
 
 const API_KEY = 'AIzaSyCJH8Y_ZExpA6SMLvSn2i0zV-R1EPYcnZA';
 
+YTSearch({key: API_KEY, term: 'surfboards'}, function (data) {
+  console.log(data);
+});
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+    this.state = { term:'surfboards'};
+    YTSearch({key: API_KEY, term: this.state.term}, function (data) {
+      console.log(data);
+    });
+
+  }
   render() {
     return (
       <div>
--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return <input onChange={event => console.log(event.target.value)}/>
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({term: event.target.value})}/>
+      </div>
+    );
   }
 }
 
